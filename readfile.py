807b3b8d2cb9d27588b084d2c05ce852a948c847
Gloria Stefania Panchana Jaramillo <gatitobebe90@gmail.com>
2024-04-09 22:11:29 -0500

    Corrección de titulo de campos y optimizacion de impresion de detalle del archivo procesado.

--- readfile.py
+++ readfile.py
@@ -1,4 +1,13 @@
-file = open("informacion.txt", mode="r")
-if file.readable():
-    print(file.read())
-file.close()
+# file = open("informacion.txt", mode="r")
+# if file.readable():
+#     print(file.read())
+# file.close()
+
+with open("informacion.txt") as archivo:
+    firtLine = archivo.readline()
+    arrayColumns = firtLine.split(";")
+    arrayColumns[3] = arrayColumns[3].replace('\n', "")
+    for linea in archivo.readlines():
+        array = linea.split(";")
+        for (i, valor) in enumerate(array):
+            print(arrayColumns[i] + ": " + array[i])
